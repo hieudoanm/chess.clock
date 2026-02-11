ac1c48e933338d81a0a6ea8a05f006c4919d4300
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-02-11 18:30:13 +0700

    add public

--- docs/_next/static/chunks/fb14bd5aaa021b07.js
+++ docs/_next/static/chunks/fb14bd5aaa021b07.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/7b6db21a05799426.js",
+  "static/chunks/28b3d5cda25c9624.js",
   "static/chunks/33878d5016030ccb.js",
   "static/chunks/6a4f38e932d8e85a.js",
   "static/chunks/712c26bcd93334b1.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-30f456031763bf7f.js"
+  "static/chunks/turbopack-f6ba59178ddb4262.js"
 ])
--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -18,7 +18,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<>
 			<Head>
-				<title>Chess</title>
+				<title>Chess Clock</title>
 			</Head>
 			<div className={`${geistSans.className} ${geistMono.className}`}>
 				<Component {...pageProps} />
